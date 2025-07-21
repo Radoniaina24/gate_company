91fc5025f275bf5fcf33382a61b19ee4277af6e4
Radoniaina24 <andriambolaradoniainamichael@gmail.com>
2025-07-21 14:43:59 +0300

    v1.0

--- src/features/Users/Liste.tsx
+++ src/features/Users/Liste.tsx
@@ -68,7 +68,7 @@ export const UsersTable = () => {
   }, []);
   useEffect(() => {
     setCurrentPage(1);
-  }, [searchTerm]);
+  }, [searchTerm, itemsPerPage]);
 
   return (
     <div className="px-4 sm:px-6 lg:px-8 py-6">
